a6199c46b53fdeaf6989d31fe959265769222603
arunabharjun <sunnyarj114@gmail.com>
2020-10-08 00:52:52 +0530

    Completed the Nav Bar

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -49,3 +49,25 @@ export const MoonIcon = ({ size = 20, classes = '' }) => {
 		</React.Fragment>
 	);
 };
+
+export const SearchIcon = ({ size = 20, classes = '' }) => {
+	return (
+		<React.Fragment>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width={size}
+				height={size}
+				viewBox='0 0 24 24'
+				fill='none'
+				stroke='currentColor'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				className={`${classes}`}
+			>
+				<circle cx={11} cy={11} r={8} />
+				<line x1={21} y1={21} x2='16.65' y2='16.65' />{' '}
+			</svg>
+		</React.Fragment>
+	);
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { setThemeDark, setThemeLight } from '../helpers/theme';
 import { ThemeContext } from '../helpers/ThemeContext';
-import { MoonIcon, SunIcon } from './Icons';
+import { MoonIcon, SunIcon, SearchIcon } from './Icons';
 
 const NavBar = () => {
 	/**
@@ -28,20 +28,88 @@ const NavBar = () => {
 		setDarkMode(!darkMode);
 	};
 
-	return (
-		<React.Fragment>
-			<nav>
-				<div className='nav-body'>
-					<div className='container'>
-						<button className='theme-toggle' onClick={toggleTheme}>
-							{darkMode && <MoonIcon />}
-							{!darkMode && <SunIcon />}
-						</button>
+	/**
+	 * rendering the theme toggle button
+	 */
+	const themeToggleButton = () => {
+		return (
+			<React.Fragment>
+				<button className='theme-toggle' onClick={toggleTheme}>
+					{/**
+					 * Show the moon icon if darkmode is active
+					 */}
+					{darkMode && <MoonIcon />}
+
+					{/**
+					 * Shoe the sun icon if darkmode is not active
+					 */}
+					{!darkMode && <SunIcon />}
+				</button>
+			</React.Fragment>
+		);
+	};
+
+	/**
+	 * rendering the brand name
+	 */
+	const brandName = () => {
+		return (
+			<React.Fragment>
+				<p className='nav-brand'>
+					{/**
+					 * Show the full brandname in desktops
+					 */}
+					<span className='name phone-hide'>
+						React <span className='accent-clr'>News</span>
+					</span>
+
+					{/**
+					 * Show the short brandname in phones
+					 */}
+					<span className='name phone-view'>
+						R<span className='accent-clr'>N</span>
+					</span>
+				</p>
+			</React.Fragment>
+		);
+	};
+
+	/**
+	 * rendering the search button
+	 */
+	const searchButton = () => {
+		return (
+			<React.Fragment>
+				<button className='search-icon'>
+					<SearchIcon />
+				</button>
+			</React.Fragment>
+		);
+	};
+
+	/**
+	 * The main navvigation bar
+	 */
+	const navBar = () => {
+		return (
+			<React.Fragment>
+				<nav>
+					<div className='nav-body'>
+						<div className='container flex-box flex-row flex-space-btw'>
+							{themeToggleButton()}
+							{brandName()}
+							{searchButton()}
+						</div>
 					</div>
-				</div>
-			</nav>
-		</React.Fragment>
-	);
+				</nav>
+			</React.Fragment>
+		);
+	};
+
+	/**
+	 * rendering the Navigation Bar
+	 */
+	return <React.Fragment>{navBar()}</React.Fragment>;
 };
 
 export default NavBar;
